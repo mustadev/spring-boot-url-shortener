583763ccac6a4981aae399b994d7ba3c51c295cf
mustadev <58852293+mustadev@users.noreply.github.com>
2020-09-12 15:45:51 +0100

    redirect controller added

---
+++ src/main/java/com/mustadev/urlshortener/controllers/RedirectController.java
@@ -0,0 +1,14 @@
+package com.mustadev.urlshortener.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedirectController {
+    
+    @GetMapping("/{text}")
+    public String redirect(@PathVariable String text){
+        return "hello Shortener " + text;
+    }
+}
