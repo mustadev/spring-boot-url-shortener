566d42d4ce48d85e850935c27e180a730e001179
mustadev <58852293+mustadev@users.noreply.github.com>
2020-09-12 18:07:40 +0100

    shorturlservice implemented

---
+++ src/main/java/com/mustadev/urlshortener/services/ShortURLServiceImp.java
@@ -0,0 +1,28 @@
+package com.mustadev.urlshortener.services;
+
+
+import com.mustadev.urlshortener.entites.ShortURL;
+import com.mustadev.urlshortener.repositores.ShortURLRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ShortURLServiceImp implements ShortURLService {
+
+    @Autowired
+    private ShortURLRepository shortURLRepository;
+    @Override
+    public ShortURL find(String shortURL) {
+        return shortURLRepository.findByShortURL(shortURL);
+    }
+
+    @Override
+    public ShortURL save(ShortURL shortURL) {
+        return shortURLRepository.save(shortURL);
+    }
+
+    @Override
+    public void delete(ShortURL shortURL) {
+       shortURLRepository.delete(shortURL);
+    }
+    
+}
