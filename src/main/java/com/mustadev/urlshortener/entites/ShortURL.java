fc93bd4aab539c84d73cb2ef05f57340ad00f56a
mustadev <58852293+mustadev@users.noreply.github.com>
2020-09-12 19:37:45 +0100

    add id field and lombok data

--- src/main/java/com/mustadev/urlshortener/entites/ShortURL.java
+++ src/main/java/com/mustadev/urlshortener/entites/ShortURL.java
@@ -1,30 +1,28 @@
 package com.mustadev.urlshortener.entites;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
+@Table(name = "shorturls")
+@Data
 public class ShortURL {
+    
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private long id;
+    
+	@Column(name = "shorturl", nullable = false, updatable = false)
     private String shortURL;
 
+    @Column(name = "longurl", nullable = false, unique = true)
     private String longURL;
-
-
-    public void setShortURL(String shortURL){
-        this.shortURL = shortURL;
-    }
-    public String getShortURL(){
-        return this.shortURL;
-    }
-
-
-    public void setlongURL(String longURL){
-        this.longURL = longURL;
-    }
-    public String getLongURL(){
-        return this.longURL;
-    }
-
 
 }
