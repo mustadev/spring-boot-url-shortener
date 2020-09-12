32a1ee28cdced52512cc14657a16f45503ee3e12
mustadev <58852293+mustadev@users.noreply.github.com>
2020-09-12 17:26:29 +0100

    redirect added

--- src/main/java/com/mustadev/urlshortener/controllers/RedirectController.java
+++ src/main/java/com/mustadev/urlshortener/controllers/RedirectController.java
@@ -1,5 +1,11 @@
 package com.mustadev.urlshortener.controllers;
 
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpHeaders;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RedirectController {
     
-    @GetMapping("/{text}")
-    public String redirect(@PathVariable String text){
-        return "hello Shortener " + text;
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Object> redirect(@PathVariable String shortUrl){
+        // TODO validate shorturl.
+        System.out.println("shrt url " + shortUrl);
+        URI url = URI.create(shortUrl);
+        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT)
+            .location(url)
+            .build();
     }
 }
