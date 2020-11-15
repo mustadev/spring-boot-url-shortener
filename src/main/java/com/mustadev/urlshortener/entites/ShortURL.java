99c33cb5dcae6b9bbb1eb382b6818c98956d1e18
mustadev <58852293+mustadev@users.noreply.github.com>
2020-11-15 02:36:00 +0100

    a remove shorturl and use id to encode and decode to short url

--- src/main/java/com/mustadev/urlshortener/entites/ShortURL.java
+++ src/main/java/com/mustadev/urlshortener/entites/ShortURL.java
@@ -17,12 +17,9 @@ public class ShortURL {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
-    private long id;
-    
-	@Column(name = "shorturl", nullable = false, updatable = false)
-    private String shortURL;
+    private int id;
 
-    @Column(name = "longurl", nullable = false, unique = true)
+    @Column(name = "longurl", nullable = false)
     private String longURL;
 
 }
