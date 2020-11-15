0c1761ed278e50367280c9e0578c35c983304886
mustadev <58852293+mustadev@users.noreply.github.com>
2020-11-15 02:36:57 +0100

    new implementation

--- src/main/java/com/mustadev/urlshortener/controllers/RedirectController.java
+++ src/main/java/com/mustadev/urlshortener/controllers/RedirectController.java
@@ -3,6 +3,7 @@ package com.mustadev.urlshortener.controllers;
 import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpHeaders;
+import java.util.HashMap;
 import java.util.Random;
 
 import com.mustadev.urlshortener.entites.ShortURL;
@@ -19,37 +20,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RedirectController {
-    
+
     @Autowired
     private ShortURLService shortURLService;
 
-    @GetMapping("/short/{shortUrl}")
-    public ResponseEntity<Object> redirect(@PathVariable String shortUrl){
+    @GetMapping("/short/{shortLink}")
+    public ResponseEntity<String> redirect(@PathVariable String shortLink) {
         // TODO validate shorturl.
-        System.out.println("shrt url " + shortUrl);
-        URI url = URI.create(shortUrl);
-        return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT)
-            .location(url)
-            .build();
+        System.out.println("shrt url " + shortLink);
+        // URI url = URI.create(shortUrl);
+        // return ResponseEntity.status(HttpStatus.PERMANENT_REDIRECT).location(url).build();
+        var id = this.decode(shortLink);
+        var shortURL = shortURLService.find(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(shortURL.getLongURL());
     }
 
-    @PostMapping()
-    public ResponseEntity<ShortURL> generateShortURL(@RequestBody String longURL){
+    @PostMapping("/long")
+    public ResponseEntity<String> generateShortURL(@RequestBody String longURL){
         ShortURL shortURL = new ShortURL();
         shortURL.setLongURL(longURL);
-        shortURL.setShortURL("localhost:8080/" + this.generateRandomString(4));
         shortURL = shortURLService.save(shortURL);
-        return ResponseEntity.status(HttpStatus.CREATED).body(shortURL);
+        var shortURLLink = this.encode(shortURL.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(shortURLLink);
     }
 
-    public  String generateRandomString(int length){
-        Random rnd = new Random();
-        String chars = "azertyuiopqsdmwxcvbn";
-        String randomString = ""; 
-        for (int i = 0; i < length; i++){
-            randomString += chars.charAt(rnd.nextInt(chars.length()));
+    
+
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int BASE = ALPHABET.length();
+
+    private String encode(int num) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            sb.append(ALPHABET.charAt(num % BASE));
+            num /= BASE;
         }
-
-        return randomString;
+        return sb.reverse().toString();
     }
+
+    private int decode(String str) {
+        int num = 0;
+        for (int i = 0; i < str.length(); i++)
+            num = num * BASE + ALPHABET.indexOf(str.charAt(i));
+        return num;
+    }
+
 }
