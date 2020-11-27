6631da4c6f03198b8b28c67bb1655a81f1268898
mustadev <58852293+mustadev@users.noreply.github.com>
2020-11-27 02:33:09 +0100

    add form handlers

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,25 +1,41 @@
-const React = require('react');
-const ReactDOM = require('react-dom'); 
+const React = require("react");
+const ReactDOM = require("react-dom");
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {longURL: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-class App extends React.Component { 
+  componentDidMount() {}
 
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
+  handleChange(event) {
+    this.setState({ longURL: event.target.value });
+  }
 
-	componentDidMount() { 
-	}
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.longURL);
+    event.preventDefault();
+  }
 
-	render() {
-		return (
-			<div> hello world  again</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="long-url"
+            type="url"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="enter valid url"
+          />
+          <input name="submit" type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('react')
-)
+ReactDOM.render(<App />, document.getElementById("react"));
